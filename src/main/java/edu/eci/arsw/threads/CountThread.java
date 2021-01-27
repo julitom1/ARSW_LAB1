ab7db09bd6ffd244052c5d9a7796ecd45f4449ef
Yarit Villalobos <yarit.villalobos@mail.escuelaing.edu.co>
2021-01-27 08:25:19 -0500

    haciendo pull

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -1,14 +1,32 @@
+package edu.eci.arsw.threads;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.eci.arsw.threads;
+
 
 /**
  *
  * @author hcadavid
  */
-public class CountThread {
-    
+
+public class CountThread extends Thread{
+	
+	private int rango1,rango2;
+	
+	public CountThread(int rango1,int rango2) {
+		rango1=this.rango1;
+		rango2=this.rango2;
+	}
+	
+	
+	
+	public void run() {
+		for(int i=rango1;i<=rango2;i++) {
+			System.out.println(i);
+		}
+	}
+
 }
--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -1,18 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.arsw.threads;
 
-/**
- *
- * @author hcadavid
- */
+import edu.eci.arsw.threads.CountThread;
+
 public class CountThreadsMain {
-    
-    public static void main(String a[]){
+	
+	public static void main(String args[]){
+    	CountThread hilo1=new CountThread(0,99);
+    	CountThread hilo2=new CountThread(99,199);
+    	CountThread hilo3=new CountThread(200,299);
+    	hilo1.start();
+    	hilo2.start();
+    	hilo3.start();
         
     }
-    
+
 }
